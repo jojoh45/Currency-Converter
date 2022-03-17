3b586436e4d9fde481ae2e9810f3027d3190ab55
Jordan <jj123813@gmail.com>
2022-03-17 11:38:21 -0400

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Jordan Johnson on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
