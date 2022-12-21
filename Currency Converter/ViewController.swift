b72e0bb2c5edf61deb18e7e2fc961989e3ada6fd
jojoh45 <jojoh45@morgan.edu>
2022-12-21 12:20:26 -0500

    Swift App Commit

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertedLabel: UILabel!
+    
+    
+    @IBOutlet weak var ConversionRateText: UITextField!
+    
+    @IBOutlet weak var AmountText: UITextField!
+    
+    @IBOutlet weak var moneyView: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        convertedLabel.text = ""
     }
-
-
+    @IBAction func convertButton(_ sender: Any) {
+        let rate = Double(ConversionRateText.text!)!
+        let amount = Double(AmountText.text!)!
+        
+        let converted = rate * amount
+        if moneyView.selectedSegmentIndex == 0{
+            convertedLabel.text = "$\(converted)"
+        }else{
+            convertedLabel.text = "¥\(converted)"
+        }
+    }
+    
+    
 }
-
